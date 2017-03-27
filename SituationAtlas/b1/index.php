a0ff37e38e49e3043086c3113184fe879f71aee6
PilErSej <xjv718@alumni.ku.dk>
2017-03-27 14:00:46 +0200

    Update index.php

--- SituationAtlas/b1/index.php
+++ SituationAtlas/b1/index.php
@@ -13,10 +13,13 @@
 	<div id="inner" class="container">
 	    <h1 id="sitat"> Situation Atlas </h1>
         <h1 id="h1"> Silly Generator </h1>
-            <form id="b1" method="post" action="/americanwhorestory.php/">
-                <button type="submit" name ="sub" value="call">American Whore Story!</buttom>
+            <form method="post" action="/americanwhorestory.php/">
+                <button id="b1"type="submit" value="call">American Whore Story</buttom>
+            </form>                
+            <br>
+            <form method="post" action="/emopoems.php/">
+                <button id="b4"  type="submit" value="call">Emopoems</buttom>
             </form>
-		</div>
 	</div>
 <div>
 
