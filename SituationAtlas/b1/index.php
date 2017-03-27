b866b96064ac6b931af2a3d40dfbe7cb0db9144e
PilErSej <xjv718@alumni.ku.dk>
2017-03-27 13:09:06 +0200

    Create index.php

---
+++ SituationAtlas/b1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<head>
+    <?php include 'style.php'; ?>
+    <style>
+    <?php include 'CSS/style.css'; ?>
+    </style>
+  <link rel='stylesheet' type='text/css' href='css/style.php' />
+  <title>Situation Atlas</title>
+</head>
+<body>
+<div id="outer" class="container">
+	<div id="inner" class="container">
+	    <h1 id="sitat"> Situation Atlas </h1>
+        <h1 id="h1"> Silly Generator </h1>
+            <form id="b1" method="post" action="/americanwhorestory.php/">
+                <button type="submit" name ="sub" value="call">American Whore Story!</buttom>
+            </form>
+		</div>
+	</div>
+<div>
+
+    
+
+</body>
+</html>
+
